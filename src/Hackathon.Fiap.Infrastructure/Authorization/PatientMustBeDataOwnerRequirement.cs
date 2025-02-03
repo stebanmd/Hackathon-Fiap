d98f225f4733c882221e28a9876c066615d4c05f
Steban Domingues <steban.domingues@poatek.com>
2025-02-03 17:28:53 -0300

    Authorization handler

---
+++ src/Hackathon.Fiap.Infrastructure/Authorization/PatientMustBeDataOwnerRequirement.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Hackathon.Fiap.Infrastructure.Authorization;
+
+public record PatientMustBeDataOwnerRequirement(int PatientId) : IAuthorizationRequirement;
