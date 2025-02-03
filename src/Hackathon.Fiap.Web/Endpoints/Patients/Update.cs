c3c323c07fb8e2869a4ce5af41ef41c5855b54a9
Steban Domingues <steban.domingues@poatek.com>
2025-02-03 17:30:31 -0300

    Add authorization handler for update method

--- src/Hackathon.Fiap.Web/Endpoints/Patients/Update.cs
+++ src/Hackathon.Fiap.Web/Endpoints/Patients/Update.cs
@@ -1,11 +1,14 @@
 ﻿using Ardalis.Result.AspNetCore;
+using Hackathon.Fiap.Infrastructure.Authorization;
 using Hackathon.Fiap.UseCases.Patients.Update;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Hackathon.Fiap.Web.Endpoints.Patients;
 
-public class Update(IMediator mediator) : Endpoint<UpdatePatientRequest>
+public class Update(IMediator mediator, IAuthorizationService authService) : Endpoint<UpdatePatientRequest>
 {
     private readonly IMediator _mediator = mediator;
+    private readonly IAuthorizationService _authService = authService;
 
     public override void Configure()
     {
@@ -14,7 +17,8 @@ public class Update(IMediator mediator) : Endpoint<UpdatePatientRequest>
         Summary(x =>
         {
             x.Response(StatusCodes.Status200OK, "Patient updated successfully.");
-            x.Response(StatusCodes.Status400BadRequest, "Invalid Payload");
+            x.Response(StatusCodes.Status400BadRequest, "Invalid Payload.");
+            x.Response(StatusCodes.Status403Forbidden, "Current user does not have access to this resource.");
             x.Response(StatusCodes.Status404NotFound, "Patient not found.");
             x.ExampleRequest = new UpdatePatientRequest("John Doe");
         });
@@ -22,6 +26,13 @@ public class Update(IMediator mediator) : Endpoint<UpdatePatientRequest>
 
     public override async Task HandleAsync(UpdatePatientRequest req, CancellationToken ct)
     {
+        var authResult = await _authService.AuthorizeAsync(User, null, new PatientMustBeDataOwnerRequirement(req.Id));
+        if (!authResult.Succeeded)
+        {
+            await SendForbiddenAsync(ct);
+            return;
+        }
+
         var command = new UpdatePatientCommand(req.Id, req.Name);
         var result = await _mediator.Send(command, ct);
 
