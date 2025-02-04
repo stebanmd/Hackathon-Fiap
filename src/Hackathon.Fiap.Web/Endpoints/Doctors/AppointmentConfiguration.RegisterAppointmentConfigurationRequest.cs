d8d136c3b16cc75981e0ba262a409a6302c53a24
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2025-02-04 19:27:44 -0300

    Fix small details

--- src/Hackathon.Fiap.Web/Endpoints/Doctors/AppointmentConfiguration.RegisterAppointmentConfigurationRequest.cs
+++ src/Hackathon.Fiap.Web/Endpoints/Doctors/AppointmentConfiguration.RegisterAppointmentConfigurationRequest.cs
@@ -4,8 +4,8 @@ namespace Hackathon.Fiap.Web.Endpoints.Doctors;
 
 public record RegisterAppointmentConfigurationRequest(double Price, double Duration)
 {
-    public const string Route = "doctors/{DoctorId:int}/appointmentConfiguration";
+    public const string Route = "doctors/{Id:int}/appointmentConfiguration";
 
     [FromRoute]
-    public int DoctorId { get; set; }
+    public int Id { get; set; }
 }
--- src/Hackathon.Fiap.Web/Endpoints/Doctors/AppointmentConfiguration.cs
+++ src/Hackathon.Fiap.Web/Endpoints/Doctors/AppointmentConfiguration.cs
@@ -4,11 +4,8 @@ using Hackathon.Fiap.UseCases.Doctors.AppointmentConfiguration;
 namespace Hackathon.Fiap.Web.Endpoints.Doctors;
 
 /// <summary>
-/// Register a new doctor in the system
+/// Register a new doctor appointment configuration
 /// </summary>
-/// <remarks>
-/// The email will be used to register a new application user granting access to restricted area
-/// </remarks>
 public partial class AppointmentConfiguration(IMediator mediator) : Endpoint<RegisterAppointmentConfigurationRequest>
 {
     private readonly IMediator _mediator = mediator;
@@ -31,7 +28,7 @@ public partial class AppointmentConfiguration(IMediator mediator) : Endpoint<Reg
 
     public override async Task HandleAsync(RegisterAppointmentConfigurationRequest req, CancellationToken ct)
     {
-        var command = new RegisterAppointmentConfigurationCommand(req.Price, req.Duration, req.DoctorId);
+        var command = new RegisterAppointmentConfigurationCommand(req.Price, req.Duration, req.Id);
         var result = await _mediator.Send(command, ct);
 
         await SendResultAsync(result.ToMinimalApiResult());
