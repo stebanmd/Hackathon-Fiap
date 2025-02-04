b276bb05b1372f466a115ff6dda9adeb14cb225b
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2025-02-04 17:49:00 -0300

    Add migration

--- src/Hackathon.Fiap.Infrastructure/Data/Migrations/20250204204627_CreateDoctorAppointmentConfigurationTable.Designer.cs
+++ src/Hackathon.Fiap.Infrastructure/Data/Migrations/20250204204627_CreateDoctorAppointmentConfigurationTable.Designer.cs
@@ -4,6 +4,7 @@ using Hackathon.Fiap.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hackathon.Fiap.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250204204627_CreateDoctorAppointmentConfigurationTable")]
+    partial class CreateDoctorAppointmentConfigurationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
