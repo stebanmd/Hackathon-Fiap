e33a0664833cfc0e28dc2fdb652c60a24e821796
Steban Domingues <steban.domingues@poatek.com>
2025-02-06 19:58:58 -0300

    Integration tests for appointments

---
+++ tests/Hackathon.Fiap.IntegrationTests/Data/Appointments/EfRepositoryAdd.cs
@@ -0,0 +1,39 @@
+﻿using Hackathon.Fiap.Core.Aggregates.Doctors;
+using Hackathon.Fiap.Core.Aggregates.Appointments;
+using Hackathon.Fiap.IntegrationTests.Fakers;
+using Hackathon.Fiap.Core.Aggregates.Patients;
+
+namespace Hackathon.Fiap.IntegrationTests.Data.Appointments;
+
+public class EfRepositoryAdd : BaseEfRepoTestFixture
+{
+    [Fact]
+    public async Task Add()
+    {
+        var start = new DateTime(2025, 3, 3, 8, 0, 0, DateTimeKind.Unspecified);
+        var end = new DateTime(2025, 3, 3, 9, 0, 0, DateTimeKind.Unspecified);
+
+        var appointment = new Appointment(start, end);
+
+        var doctorFaker = new DoctorFaker().Generate();
+        var doctor = new Doctor(doctorFaker.Name, doctorFaker.Cpf, doctorFaker.Crm);
+        doctor.SetUser(doctorFaker.User);
+
+        var patientFaker = new PatientFaker().Generate();
+        var patient = new Patient(patientFaker.Name, patientFaker.Cpf);
+        patient.SetUser(patientFaker.User);
+
+        appointment.SetPatient(patient);
+        appointment.SetDoctor(doctor);
+
+        var repository = GetRepository<Appointment>();
+
+        await repository.AddAsync(appointment);
+        
+        Assert.Equal(appointment.Start, start);
+        Assert.Equal(appointment.End, end);
+        Assert.True(appointment.Id > 0);
+        Assert.Equal(AppointmentStatus.Pending, appointment.Status);
+
+    }
+}
