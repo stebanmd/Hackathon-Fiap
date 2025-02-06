36dc9617e06cd7e1e1991d10a3b7a3acd1d87145
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2025-02-06 19:09:50 -0300

    Handling reply appointment event

---
+++ src/Hackathon.Fiap.Core/Aggregates/Appointments/Handlers/ReplyAppointmentHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using Hackathon.Fiap.Core.Aggregates.Appointments.Events;
+using Hackathon.Fiap.Core.Interfaces;
+
+namespace Hackathon.Fiap.Core.Aggregates.Appointments.Handlers;
+internal class ReplyAppointmentHandler(IEmailSender emailSender, ILogger<ReplyAppointmentHandler> logger) : INotificationHandler<ReplyAppointmentEvent>
+{
+    private readonly IEmailSender _emailSender = emailSender;
+    private readonly ILogger<ReplyAppointmentHandler> _logger = logger;
+
+    public async Task Handle(ReplyAppointmentEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Appointment was {Status}. Sending email notifying the patient", notification.Appointment.Status);
+
+        var appointment = notification.Appointment;
+        var doctor = appointment.Doctor;
+        var patient = appointment.Patient;
+
+        var statusMessage = appointment.Status switch
+        {
+            AppointmentStatus.Pending => "Pendente de confirmação",
+            AppointmentStatus.Confirmed => "Confirmada",
+            AppointmentStatus.Canceled => "Cancelada",
+            _ => throw new NotImplementedException(),
+        };
+
+        var emailBody = new StringBuilder();
+        emailBody.AppendLine($"Olá, {patient.Name}!");
+        emailBody.AppendLine($"Sua consulta agendadata para {appointment.Start:dd/MM/yyyy} às {appointment.Start:HH:mm}, com o doutor {doctor.Name} teve o status alterado para {statusMessage}.");
+        if (!string.IsNullOrEmpty(appointment.Reason))
+        {
+            emailBody.AppendLine($"Motivo:");
+            emailBody.AppendLine(appointment.Reason);
+        }
+
+        await _emailSender
+            .SendEmailAsync(patient.User.Email!,
+                            "noreply@healthmed.com",
+                            "Health&Med - Resposta de agendamento de consulta",
+                            emailBody.ToString());
+    }
+}
