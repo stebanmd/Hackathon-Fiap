4ecd8f75ab84763bad534b85d24f91067086e38e
Steban Domingues <steban.domingues@poatek.com>
2025-02-06 11:23:53 -0300

    Add Reason column into Appointments table

---
+++ src/Hackathon.Fiap.Infrastructure/Data/Migrations/20250206132922_AddReasonColumnInAppointments.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon.Fiap.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class AddReasonColumnInAppointments : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Reason",
+            table: "Appointments",
+            type: "nvarchar(500)",
+            maxLength: 500,
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Reason",
+            table: "Appointments");
+    }
+}
